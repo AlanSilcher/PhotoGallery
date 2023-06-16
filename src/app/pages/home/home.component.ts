58fcb1d7fbe3e755f97bb5751ab3d0874350ac3e
AlanSilcher <silcheralan@gmail.com>
2023-06-16 09:52:03 -0300

    Home completo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -30,22 +30,35 @@ export class HomeComponent {
     const numColumns = 3;
     const imagesPerColumn = Math.ceil(images.length / numColumns);
 
-    this.columns = [];
-
-    for (let i = 0; i < numColumns; i++) {
-      const startIndex = i * imagesPerColumn;
-      const endIndex = startIndex + imagesPerColumn;
-      const columnImages = images.slice(startIndex, endIndex);
-      this.columns.push(columnImages);
+    if (this.columns.length === 0) {
+      // Si no hay columnas existentes, crear nuevas columnas con las imágenes
+      for (let i = 0; i < numColumns; i++) {
+        const startIndex = i * imagesPerColumn;
+        const endIndex = startIndex + imagesPerColumn;
+        const columnImages = images.slice(startIndex, endIndex);
+        this.columns.push(columnImages);
+      }
+    } else {
+      // Agregar las nuevas imágenes a las columnas existentes
+      let columnIndex = 0;
+      for (let i = 0; i < images.length; i++) {
+        this.columns[columnIndex].push(images[i]);
+        columnIndex = (columnIndex + 1) % numColumns;
+      }
     }
   }
 
   loadMoreImages() {
-    this.fetchImages();
+    const apiUrl = `https://api.unsplash.com/photos/random?count=${this.imagesPerPage}&client_id=Y-qrJ1ErRuz4CigR6nD19M4ad76OLJyaKm-TAT7xl18`;
+
+    this.http.get<Object[]>(apiUrl).subscribe((data: Object[]) => {
+      const newImages = data.map((item: any) => ({ url: item.urls.small, id: item.id }));
+      this.arrangeImagesInColumns(newImages);
+    });
   }
 
   canLoadMoreImages(): boolean {
-    return this.columns.length === 0 || this.columns[0].length === this.imagesPerPage;
+    return this.columns.length > 0;
   }
 
   redirectToDetail(id: string) {
