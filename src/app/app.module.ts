e409cb3c160f22f3a466f5ba6c92ee4e965df8c6
AlanSilcher <silcheralan@gmail.com>
2023-06-16 17:41:58 -0300

    Ajuste vercel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http'; 
 import { FormsModule } from '@angular/forms';
-import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -36,7 +35,6 @@ const routes: Routes = [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    SlickCarouselModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -6,13 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent {
-  images = [
-    { url: 'https://images.unsplash.com/photo-1686391640847-e08e425127f4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80', id: 'imagen1' },
-    { url: 'https://plus.unsplash.com/premium_photo-1684966120297-499ef1975987?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80', id: 'imagen2' },
-    { url: 'https://images.unsplash.com/photo-1686465606900-d8b7724aa356?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80', id: 'imagen3' }
-  ];
-
-  redirectToDetail(id: string) {
-    // Lógica para redireccionar a detalles de la imagen
-  }
+  
 }
